dfc76432e1b75e35ef36e603fa840d16cbe51bef
alucard93 <m.vinicius93.brasilmortal@gmail.com>
2023-05-06 00:19:37 -0300

    feat: task
    - criação da estrutura do componente ButtonTask;
    - estilização inicial do componente ButtonTask.

--- src/components/Task/ButtonTask/ButtonTask.tsx
+++ src/components/Task/ButtonTask/ButtonTask.tsx
@@ -1,6 +1,18 @@
-
+import { ButtonTaskStyled } from "./styles";
+import { Trash } from "phosphor-react";
 export const ButtonTask = () => {
   return (
-    <div>ButtonTask</div>
-  )
-}
+    <ButtonTaskStyled>
+      <div className="content-button-task">
+        <input title="marcar como concluído" className="checkbox" type="checkbox" />
+        <p>
+          Integer urna interdum massa libero auctor neque turpis turpis semper.
+          Duis vel sed fames integer.
+        </p>
+      </div>
+      <div title="deletar tarefa" className="content-trash">
+        <Trash size={16} />
+      </div>
+    </ButtonTaskStyled>
+  );
+};
--- src/components/Task/style.ts
+++ src/components/Task/style.ts
@@ -2,5 +2,6 @@ import styled from "styled-components";
 
 export const TaskStyled = styled.div`
   flex-direction: column;
+  gap: 24px;
   
 `
